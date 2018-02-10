bcef588cd9e9bcbf888bd67ba8b99468518ccc5e
Eliot Szwajkowski <eliotpszw@gmail.com>
2018-02-10 12:09:05 -0500

    Review I

---
+++ Review I/1801B-ReviewI.js
@@ -0,0 +1,12 @@
+const chalk = require('chalk');
+
+console.log(chalk.red('------------------------------'));
+
+// From the interwebs:
+
+/*
+Sharon:
+Hi Eliot! Unfortunately I can’t make this review session .. but I was hoping you can kind of go over how to use VSCode as well as GitHub.
+Can you also go over template laterals?
+And sorry if I missed this but - how do you determine whether or not some variable is an immutable value..?
+*/
